51237e0edfc9a0c7e973ad38f4f7004ee75ae3bc
meenukunj <athulk670@gmail.com>
2023-06-20 16:32:17 +0530

    Initial commit

---
+++ progam 1.py
@@ -0,0 +1,9 @@
+a=int(input("enter:"))
+b=int(input("enter:"))
+c=int(input("enter:"))
+if(a>b and a>c):
+    print(a,"is greater")
+elif(b>c and b>a):
+    print(b," is greater") 
+elif(c>a and c>b):
+    print(c," is greater")
---
+++ program 3.py
@@ -0,0 +1,8 @@
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+print("sum =",x+y)
+print("diff ="x-y)
+print("mul =",x*y)
+print("div =",x/y)
+print("mod =",x%y)
+print("float div=",y//x)
